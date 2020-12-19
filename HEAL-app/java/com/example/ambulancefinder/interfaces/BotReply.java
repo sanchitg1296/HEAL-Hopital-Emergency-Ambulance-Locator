959b478c751f81da94272e2bf160f81161d8f882
mokshdagangrade <68953506+mokshdagangrade@users.noreply.github.com>
2020-12-20 00:58:54 +0530

    Delete BotReply.java

--- HEAL-app/java/com/example/ambulancefinder/interfaces/BotReply.java
+++
@@ -1,7 +0,0 @@
-package com.example.ambulancefinder.interfaces;
-
-import com.google.cloud.dialogflow.v2.DetectIntentResponse;
-
-public interface BotReply {
-    void callback(DetectIntentResponse returnResponse);
-}
